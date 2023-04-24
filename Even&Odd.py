66fa176a2eb151f6e899a05406cc62f124d65889
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-24 22:21:50 +0800

    read numbers.txt line by line

--- Even&Odd.py
+++ Even&Odd.py
@@ -1,3 +1,7 @@
 def process():
     # open numbers.txt(read), even.txt(append), odd.txt(append)
-        with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+    with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+        # read numbers.txt line by line
+        for line in input_file:
+            input_num = int(line)
+            
