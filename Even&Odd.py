42eee1aa216c6d5a62d99267c603b971afb1f0a1
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-24 23:07:22 +0800

    It's workinnggg!

--- Even&Odd.py
+++ Even&Odd.py
@@ -1,3 +1,7 @@
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers.
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt
+# The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt
+
 def process():
     # open numbers.txt(read), even.txt(append), odd.txt(append)
     with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
